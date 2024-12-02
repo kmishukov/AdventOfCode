91e4816f6c557321db583fce1b7636acb50af66a
Konstantin <kkmishukov@gmail.com>
2024-12-02 04:03:57 +0300

    added step 2 for 2024-1

--- 2024/1. Historian Hysteria/1. Historian Hysteria/main.swift
+++ 2024/1. Historian Hysteria/1. Historian Hysteria/main.swift
@@ -9,11 +9,25 @@ import Foundation
 
 func main() {
     let (arr1, arr2) = DataMapper.mapInputs(Input.text)
-    var diff = 0
+//    var diff = 0
+//    for i in 0..<arr1.count {
+//        diff += abs(arr1[i] - arr2[i])
+//    }
+//    print(diff)
+
+    var similarity = 0
+    var countDict: [Int: Int] = [:]
     for i in 0..<arr1.count {
-        diff += abs(arr1[i] - arr2[i])
+        if let count = countDict[arr1[i]] {
+            similarity += arr1[i] * count
+            continue
+        } else {
+            let count = arr2.compactMap { $0 == arr1[i] ? $0 : nil }.count
+            countDict[arr1[i]] = count
+            similarity += arr1[i] * count
+        }
     }
-    print(diff)
+    print(similarity)
 }
 
 main()
